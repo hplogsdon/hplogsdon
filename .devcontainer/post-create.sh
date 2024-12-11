1aa4f00b72b4b446e8d2ebf3d08f2cfdbaf1747d
uxp <uxp@bsdeviant.org>
2024-12-11 18:46:20 +0000

    Github actions to deploy and more building fixes

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,14 @@ set -eu -o pipefail
 
 git config --global --add safe.directory .
 
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock >/dev/null; then
+# Install the version of Ruby specified in .ruby-version.
+if [ -f .ruby-version ]; then
+    rvm install -s ruby-$(cat .ruby-version)
+    rvm use $(cat .ruby-version)
+fi
+
+# Install the version of Bundler.
+if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
@@ -13,3 +20,4 @@ if [ -f Gemfile ]; then
     bundle install
 fi
 
+bundle exec jekyll serve --force-polling --baseurl=''
